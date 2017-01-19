ad92942aae89220a7259b0758b7150110b41d1f0
Dewiter <herolezero@gmail.com>
2017-01-19 18:59:04 +0100

    added a pivoting wall

--- Assets/script/Obstacle/Obstacle.cs
+++ Assets/script/Obstacle/Obstacle.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour {	
 	void FixedUpdate () 
 	{
-		if (gameObject.tag == "Left")
-		{		
-			transform.Translate(Vector3.forward *  Time.deltaTime);
-		}
-		if (gameObject.tag == "Right")
-		{		
-			transform.Translate((Vector3.forward * -1) *  Time.deltaTime);
-		}
+		transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
 	}
 }
